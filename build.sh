7895f2d5cb1b30a480537d55761e2ab644d6b5af
generalmab3 <124401932+generalmab3@users.noreply.github.com>
2026-09-11 14:38:08 +0330

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ساخت پایان‌نامه با زی‌لاتک (دو بار اجرا برای فهرست‌ها و ارجاع‌ها)
-set -e
-cd "$(dirname "$0")"
-xelatex -interaction=nonstopmode thesis.tex
-xelatex -interaction=nonstopmode thesis.tex
-echo "Done: thesis.pdf"
