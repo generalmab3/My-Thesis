5a62333596d2f87f1af7d9735f3150539db5c462
generalmab3 <124401932+generalmab3@users.noreply.github.com>
2026-09-11 13:49:12 +0330

    Delete defense directory

--- defense/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ساخت اسلایدها با زی‌لاتک
-set -e
-cd "$(dirname "$0")"
-xelatex -interaction=nonstopmode slides.tex
-xelatex -interaction=nonstopmode slides.tex
-echo "Done: slides.pdf"
